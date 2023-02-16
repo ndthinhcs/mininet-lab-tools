b56f8ff3ced57053483524d1cec464af2e7b8a1e
Thinh Ngo <lizadon321@gmail.com>
2023-02-16 17:17:37 +0700

    them

--- pcap-capture/scenario/youtube.py
+++ pcap-capture/scenario/youtube.py
@@ -1,18 +1,30 @@
-from webpcap.webpcap.pcapcapture import *
-from webpcap.webpcap.ggservice import *
-from logging import warn, error, info, debug
+from webpcap.pcapcapture import QUICTrafficCapture
+from webpcap.ggservice import YoutubePlayer
+from logging import warn, error, info, debug, critical
 import os, sys
 import pandas as pd
+import tomli
+
+try:
+    with open('config.toml', 'rb') as f:
+        config = tomli.load(f)
+        sys.path.insert(0, '/home/onos/Documents/Github/mininet-lab-tools/pcap-capture/webpcap' )
+except FileNotFoundError:
+    print('Config file not found')
+    exit(1)
 
 def main():
     df = pd.read_csv("")
     df_link = df['Links']
     for i in range(len(df_link)):
-        get_link = YoutubeLivePlayer(df_link[i])
+        get_link = YoutubePlayer(df_link[i])
         get_link.load(df_link[i])
         get_link.play_button()
-
+        
+        
 if __name__ == '__main__':
+    capture = QUICTrafficCapture('ens33')
+    capture.capture()
     try:
         main()
     except KeyboardInterrupt:
